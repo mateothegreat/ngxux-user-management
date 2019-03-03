e6f54acbe1e77715938fb199441889bbf899d65a
Matthew Davis <matthew@matthewdavis.io>
2019-03-03 08:23:25 -0600

    export User class

--- src/public_api.ts
+++ src/public_api.ts
@@ -6,3 +6,4 @@ export * from './lib/ngxux-user-management.service';
 export * from './lib/ngxux-user-management.component';
 export * from './lib/ngxux-user-management.module';
 export * from './lib/ngxux-user-management-config';
+export * from './lib/user';
